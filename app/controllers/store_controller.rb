0a7f0554b23c7b08677d3263c1fb3a1daf5f15ec
sarahbkim <pnkdaegee@gmail.com>
2015-05-30 11:07:34 -0700

    add line items to carts

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+  
   def index
   	@products = Product.all.order(:title)
+    @count = session_count
+  end
+
+  def session_count
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
   end
 end
