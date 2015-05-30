c4ebbf59eb214d035203ddf7df7caac866d3cd6c
sarahbkim <pnkdaegee@gmail.com>
2015-05-30 11:48:36 -0700

    add error msgg

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -25,7 +25,9 @@ class CartsController < ApplicationController
   # POST /carts.json
   def create
     @cart = Cart.new(cart_params)
-  
+    product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(product.id)
+
     respond_to do |format|
       if @cart.save
         format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
@@ -65,7 +67,17 @@ class CartsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
-      @cart = Cart.find(params[:id])
+      begin
+        @cart = Cart.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        logger.error "Attempt to access invalid cart #{params[:id]}"
+        redirect_to store_url, notice: 'Invalid cart'
+      else
+        respond_to do |format|
+          format.html # show.html.erb
+          format.json { render json: @cart }
+        end
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
