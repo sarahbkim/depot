5f9f437c113108b9b708634f0654ce8f8b635760
sarahbkim <pnkdaegee@gmail.com>
2015-05-30 12:04:18 -0700

    updated cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -57,9 +57,12 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
+
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: 'Cart was successfully destroyed.' }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
@@ -72,11 +75,6 @@ class CartsController < ApplicationController
       rescue ActiveRecord::RecordNotFound
         logger.error "Attempt to access invalid cart #{params[:id]}"
         redirect_to store_url, notice: 'Invalid cart'
-      else
-        respond_to do |format|
-          format.html # show.html.erb
-          format.json { render json: @cart }
-        end
       end
     end
 
